cc1f43a4aff416d54fdde6ac787fc56081f6f197
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-10-30 14:21:00 +0000

    Copied Tim's example, making sure I understand everything

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,50 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 
+// I've struggled with React so far so I'm gonna comment EVERYTHING I'm not 100% sure on so I know what it does
+
+// Define the API key, which is in .env to keep it secret
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  return <></>;
+  // search has a state of an empty string, until setSearch does something
+  const [search, setSearch] = useState("");
+
+  // Function that listens for input in the form and updates search state to the input text
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  // location has a state of an empty object, until setLocation does something
+  const [location, setLocation] = useState({});
+
+  // Function to get location when form is submitted; it's asynchrounous
+  // preventDefault stops the form's submitted info being added to the URL
+  async function getLocation(event) {
+    event.preventDefault();
+
+    // The API URL that we're getting out results from, self explanatory
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    // Get result from the API, use "await" as it takes time (asynchronous!)
+    const result = await axios.get(API);
+
+    // Pull the first piece of info from the result and update location state
+    setLocation(result.data[0]);
+  }
+
+  // All of the below makes sense
+  return (
+    <>
+      <h1>City Guys</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Type a location" />
+        <button>Get Location</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+    </>
+  );
 }
 
 export default App;
